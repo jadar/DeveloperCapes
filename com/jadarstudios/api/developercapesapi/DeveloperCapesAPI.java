14d22ee869847affb4f31e889829ce7ef482c561
Captain of all Shadows <captain_shadows@aol.com>
2013-04-21 01:28:44 -0400

    It looks like I deleted a lot but that is just github being weird.
    Removed System.out.println and made the comments better :P

--- com/jadarstudios/api/developercapesapi/DeveloperCapesAPI.java
+++ com/jadarstudios/api/developercapesapi/DeveloperCapesAPI.java
@@ -2,7 +2,6 @@
  * Copyright (c) Jadar, 2013
  * Developer Capes API by Jadar
  * License: Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
- * 
  * version 1.3.1
  */
 package com.jadarstudios.api.developercapesapi;
@@ -15,7 +14,6 @@ import java.util.HashMap;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.ImageBufferDownload;
-
 import cpw.mods.fml.common.registry.TickRegistry;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -23,127 +21,137 @@ import cpw.mods.fml.relauncher.SideOnly;
 @SideOnly(Side.CLIENT)
 public final class DeveloperCapesAPI {
 
-	private static DeveloperCapesAPI instance;
+    private static DeveloperCapesAPI instance;
 
-	private static final Minecraft mc = Minecraft.getMinecraft();
-	
-	private HashMap<String, String> users;
-	private HashMap<String, String> groupUrls;
+    private static final Minecraft mc = Minecraft.getMinecraft();
 
-	private boolean tickSetUp = false;
-	
-	/**
-	 * Object constructor.
-	 * 
-	 * @param parTxtUrl
-	 * @param parDeveloperCape
-	 * @param parTesterCape
-	 */
-	private DeveloperCapesAPI() {
-		users = new HashMap<String, String>();
-		groupUrls = new HashMap<String, String>();
-	}
+    private HashMap<String, String> users;
+    private HashMap<String, String> groupUrls;
 
-	public static DeveloperCapesAPI getInstance() {
-		if(instance == null) {
-			instance = new DeveloperCapesAPI();
-		}
-		return instance;
-	}
+    private boolean tickSetUp = false;
 
-	/**
-	 * Set up capes. All cape urls are in the txt file passed in. 
-	 * https://github.com/jadar/DeveloperCapesAPI/blob/master/SampleCape.txt
-	 * 
-	 * @param parTxtUrl
-	 */
-	public void init(String parTxtUrl) {
-		try {
-			URL url = new URL(parTxtUrl);
-			BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-			String line;
+    /**
+     * Object constructor.
+     */
+    private DeveloperCapesAPI() {
+        users = new HashMap<String, String>();
+        groupUrls = new HashMap<String, String>();
+    }
 
-			String username = "";
-			String group = "";
-			String capeUrl = "";
-			System.out.println(parTxtUrl);
-			while((line = reader.readLine()) != null) {
-				
-				// excludes commented lines
-				if(!line.startsWith("#")) {
-					// loops through characters.
-					for(int i=0; i<line.length(); i++) {
-						// when char : is found do stuff.
-						if(line.charAt(i) == '=') {
-							group = line.substring(0, i);
-							String subLine = line.substring(i+1);
-							
-							if(subLine.startsWith("http")) {
-								capeUrl = subLine;
-								getInstance().addGroupUrl(group, capeUrl);
-								mc.renderEngine.obtainImageData(capeUrl, new ImageBufferDownload());
-								System.out.println(capeUrl);
-								continue;
-							}
-							else {
-								username = subLine.toLowerCase();
-								getInstance().addUser(username, group);
-							}
-						}
-					}
-				}
-			}
-		}
-		catch(IOException x) {
-			x.printStackTrace();
-		}
+    /**
+     * Get's the current DeveloperCapesAPI instance, or creates a new one if
+     * necessary.
+     */
+    public static DeveloperCapesAPI getInstance() {
+        if (instance == null){
+            instance = new DeveloperCapesAPI();
+        }
+        return instance;
+    }
 
-		// make sure to set up only one tick handler.
-		if(!instance.tickSetUp) {
-			// set up tick handler for capes.
-			TickRegistry.registerTickHandler(new DeveloperCapesTickHandler(), Side.CLIENT);
-			instance.tickSetUp = true;
-		}
-	}
+    /**
+     * Set up capes. All cape URLs are in the txt file passed in.
+     * https://github.com/jadar/DeveloperCapesAPI/blob/master/SampleCape.txt
+     * 
+     * @param parTxtUrl
+     *            The URL of the .txt file containing the groups, members of
+     *            said groups, and the group's cape URL.
+     */
+    public void init(String parTxtUrl) {
+        try{
+            URL url = new URL(parTxtUrl);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            String line;
 
-	/**
-	 * Used to add user to users HashMap.
-	 * @param parUsername
-	 * @param parGroup
-	 */
-	public void addUser(String parUsername, String parGroup) {
-		if(getUserGroup(parUsername) == null) {
-			users.put(parUsername, parGroup);
+            String username = "";
+            String group = "";
+            String capeUrl = "";
+            // System.out.println(parTxtUrl);
+            while ((line = reader.readLine()) != null){
 
-		}
-	}
+                // excludes commented lines
+                if (!line.startsWith("#")){
+                    // loops through characters.
+                    for (int i = 0; i < line.length(); i++){
+                        // when char : is found do stuff.
+                        if (line.charAt(i) == '='){
+                            group = line.substring(0, i);
+                            String subLine = line.substring(i + 1);
 
-	/**
-	 * Used to get user from users HashMap.
-	 * @param parUsername
-	 * @return
-	 */
-	public String getUserGroup(String parUsername)  {
-		return users.get(parUsername.toLowerCase());
-	}
+                            if (subLine.startsWith("http")){
+                                capeUrl = subLine;
+                                getInstance().addGroupUrl(group, capeUrl);
+                                mc.renderEngine.obtainImageData(capeUrl, new ImageBufferDownload());
+                                // System.out.println(capeUrl);
+                                continue;
+                            }else{
+                                username = subLine.toLowerCase();
+                                getInstance().addUser(username, group);
+                            }
+                        }
+                    }
+                }
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
 
-	/**
-	 * Used to add group and url to groupUrls HashMap.
-	 * @param parGroup
-	 * @param parCapeUrl
-	 */
-	public void addGroupUrl(String parGroup, String parCapeUrl) {
-		if(getGroupUrl(parGroup) == null) {
-			groupUrls.put(parGroup, parCapeUrl);
-		}
-	}
+        // Makes sure to set up only one tick handler.
+        if (!instance.tickSetUp){
+            // Sets up the tick handler for capes.
+            TickRegistry.registerTickHandler(new DeveloperCapesTickHandler(), Side.CLIENT);
+            instance.tickSetUp = true;
+        }
+    }
 
-	/**
-	 * Used to get url from groupUrl by the group name.
-	 * @param group
-	 * @return
-	 */
-	public String getGroupUrl(String group) {
-		return groupUrls.get(group);
-	}
+    /**
+     * Used to add user to users HashMap.
+     * 
+     * @param parUsername
+     *            The Username to add.
+     * @param parGroup
+     *            The group to add that Username to.
+     */
+    public void addUser(String parUsername, String parGroup) {
+        if (getUserGroup(parUsername) == null){
+            users.put(parUsername, parGroup);
+
+        }
+    }
+
+    /**
+     * Used to get user from users HashMap.
+     * 
+     * @param parUsername
+     *            The Username to get from the users HashMap.
+     * @return The Username found in the users HashMap.
+     */
+    public String getUserGroup(String parUsername) {
+        return users.get(parUsername.toLowerCase());
+    }
+
+    /**
+     * Used to add group and URL to groupUrls HashMap.
+     * 
+     * @param parGroup
+     *            The group to add.
+     * @param parCapeUrl
+     *            The corresponding URL to add.
+     */
+    public void addGroupUrl(String parGroup, String parCapeUrl) {
+        if (getGroupUrl(parGroup) == null){
+            groupUrls.put(parGroup, parCapeUrl);
+        }
+    }
+
+    /**
+     * Used to get URL from groupUrl by the group name.
+     * 
+     * @param group
+     *            The name of the group to get the URL from.
+     * @return The group URL.
+     */
+    public String getGroupUrl(String group) {
+        return groupUrls.get(group);
+    }
 }
