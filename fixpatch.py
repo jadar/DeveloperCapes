f37b4def4259da084d02e10ef153f6cfbeab9d08
Jacob Rhoda <osx86geek@gmail.com>
2013-07-22 16:14:48 -0500

    added python script that will fix the line endings and such of a patch file. i did not write the fix_patch function, it was taken from fml.py of the forge installation scripts.

---
+++ fixpatch.py
@@ -0,0 +1,65 @@
+﻿import os, os.path
+import sys, getopt
+
+def main(argv):
+   
+   inputfile = ''
+   outputfile = ''
+   try:
+      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+   except getopt.GetoptError:
+      print 'fixpatch.py -i <inputfile> -o <outputfile>'
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print 'fixpatch.py -i <inputfile> -o <outputfile>'
+         sys.exit()
+      elif opt in ("-i", "--ifile"):
+         inputfile = arg
+      elif opt in ("-o", "--ofile"):
+         outputfile = arg
+
+   # actually runs the patch patcher.
+   fix_patch(inputfile, outputfile)
+
+#Taken from fml.py. written by MinecraftForge people.
+def fix_patch(in_file, out_file, find=None, rep=None):
+    #Fixes the following issues in the patch file if they exist:
+    #  Normalizes the path seperators for the current OS
+    #  Normalizes the line endings
+    # Returns the path that the file wants to apply to
+    
+    in_file = os.path.normpath(in_file)
+    if out_file is None:
+        tmp_file = in_file + '.tmp'
+    else:
+        out_file = os.path.normpath(out_file)
+        tmp_file = out_file
+        dir_name = os.path.dirname(out_file)
+        if dir_name:
+            if not os.path.exists(dir_name):
+                os.makedirs(dir_name)
+                
+    file = 'not found'
+    with open(in_file, 'rb') as inpatch:
+        with open(tmp_file, 'wb') as outpatch:
+            for line in inpatch:
+                line = line.rstrip('\r\n')
+                if line[:3] in ['+++', '---', 'Onl', 'dif']:
+                    if not find == None and not rep == None:
+                        line = line.replace('\\', '/').replace(find, rep).replace('/', os.sep)
+                    else:
+                        line = line.replace('\\', '/').replace('/', os.sep)
+                    outpatch.write(line + os.linesep)
+                else:
+                    outpatch.write(line + os.linesep)
+                if line[:3] == '---':
+                    file = line[line.find(os.sep, line.find(os.sep)+1)+1:]
+                    
+    if out_file is None:
+        shutil.move(tmp_file, in_file)
+    return file
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+   
