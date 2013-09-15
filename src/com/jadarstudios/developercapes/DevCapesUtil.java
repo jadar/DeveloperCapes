dd906625c08efc36790d32a8a559092251117e26
jadar <jake@jadarstudios.com>
2013-09-15 09:32:27 -0500

    removed code that prevented minecraft servers to load.

--- src/com/jadarstudios/developercapes/DevCapesUtil.java
+++ src/com/jadarstudios/developercapes/DevCapesUtil.java
@@ -23,7 +23,6 @@ import cpw.mods.fml.common.registry.TickRegistry;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-@SideOnly(Side.CLIENT)
 public class DevCapesUtil {
 
 	private static DevCapesUtil instance;
