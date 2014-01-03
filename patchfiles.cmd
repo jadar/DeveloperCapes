82b15cad60670e5fee843d325cae4dca4d876655
jadar <jake@jadarstudios.com>
2014-01-03 14:57:48 -0600

    no more at

--- patchfiles.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-IF "%1"=="-h" echo "Usage: patchfiles.cmd <mcpdirectory>"
-IF [%1]==[] (
-	echo "Usage: patchfiles.cmd <mcpdirectory>"
-)
-IF EXIST "%1\runtime\bin\python\python_mcp.exe" %1\runtime\bin\python\python_mcp.exe patchfiles.py -d %1
-PAUSE
